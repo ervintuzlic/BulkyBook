10de806d51cb372bde06492b0cda216329467d9d
Ervin Tuzlic <ervin@fragnet.net>
2022-06-05 13:08:22 +0200

    Create and connect Database

--- BulkyBookWeb/Data/ApplicationDBContext.cs
+++ BulkyBookWeb/Data/ApplicationDBContext.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Data
 {
     public class ApplicationDBContext : DbContext
     {
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
-
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
